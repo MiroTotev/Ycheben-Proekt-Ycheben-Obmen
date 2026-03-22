931704fdea9da135ccd8fcd1507090b9c8a96dc3
Unknown1Zq <illusiongh123@gmail.com>
2026-03-22 19:51:59 +0200

    Create index.php for home page with navigation menu

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home Page</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="upload.php">Upload</a></li>
+            <li><a href="resources.php">Resources</a></li>
+            <li><a href="chat.php">Chat</a></li>
+            <li><a href="logout.php">Logout</a></li>
+        </ul>
+    </nav>
+    <h1>Welcome to the Home Page</h1>
+</body>
+</html>
