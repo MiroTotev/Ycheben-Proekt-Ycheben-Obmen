01f7534a47182dd386a66ead07bb5a71e497908b
Unknown1Zq <illusiongh123@gmail.com>
2026-03-22 19:50:24 +0200

    Created config.php for database configuration

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+// Database configuration 
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "dbname";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
